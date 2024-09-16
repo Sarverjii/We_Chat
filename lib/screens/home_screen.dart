58e15503582bfacc148bc83a16bfd7dd00d19f96
Sarverjii <sarverjii@gmail.com>
2024-09-17 02:49:12 +0530

    Added Appbar and Floating Button

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("We Chat"),
+        leading: const Icon(Icons.home),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add_comment_outlined,
+        ),
+      ),
+      body: const Center(
+        child: Text("We Chat"),
+      ),
+    );
+  }
+}
